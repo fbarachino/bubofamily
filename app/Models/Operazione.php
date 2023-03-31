6c5c6e133b54ba1eb6df945ffddc646d83a9eaa6
Flavio Barachino <flavio.barachino@gmail.com>
2023-03-31 12:12:45 +0200

    Pulizia

--- app/Models/Operazione.php
+++ app/Models/Operazione.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use App\Models\Auto;
-use App\Models\Categorie;
 
 class Operazione extends Model
 {
--- database/migrations/2022_03_11_110826_create_events_table.php
+++ database/migrations/2022_03_11_110826_create_events_table.php
@@ -18,13 +18,9 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
 
             $table->id();
-
             $table->string('title');
-
             $table->date('start');
-
             $table->date('end');
-
             $table->timestamps();
 
         });
