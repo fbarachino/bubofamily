5fed6fdf1615f4961e4b0cf3fde9a92baf16c749
Flavio Barachino <flavio.barachino@gmail.com>
2023-05-07 07:12:39 +0200

    Modificato Javascript per nascondere il form e i dettagli del progetto

--- resources/views/progetti/dettaglio.blade.php
+++ resources/views/progetti/dettaglio.blade.php
@@ -1,153 +1,162 @@
-@extends('admin')
-@section('content')
+@extends('admin') @section('content')
 <div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">Dettaglio Progetto</h1>
-    </div>
-</div>                          
-	<div class="container">
-    	<!-- Content here -->
-    	<div class="row">
-            <div class="col-xs-12">
-            	<a class="btn btn-primary" href="progetti/new"><i class="fa fa-pencil-square-o fw"></i></a>
-            </div>
-        </div>	
-    	
-    <div class="row">
-     	<div class="col-xs-10">
-        	<div class="panel panel-default">
-            	<div class="panel-heading" id="dettaglio">
-                    Dettaglio Progetto
-                </div>
-                <div class="panel-body"> 
-                @foreach($dettaglio ?? '' as $progetto)
-                	<div class="row">
-     					<div class="col-xs-2">
-        					Nome:
-        				</div>
-        				<div class="col-xs-8">
-        					<b>{{ $progetto->nome; }}</b>
-        				</div>
-        			</div>
-
-                	<div class="row" hidable="">
-     					<div class="col-xs-2">
-        					Descrizione
-        				</div>
-        				<div class="col-xs-8">
-        					<b>{{ $progetto->descrizione; }}</b>
-        				</div>
-        			</div>
-        			<div class="row" hidable="">
-     					<div class="col-xs-2">
-        					Data Inizio
-        				</div>
-        				<div class="col-xs-3">
-        					<b>{{  date('d/m/Y',strtotime($progetto->data_inizio)) }}</b>
-        				</div>
-     					<div class="col-xs-2">
-        					Data Termine
-        				</div>
-        				<div class="col-xs-3">
-        					<b>{{  date('d/m/Y',strtotime($progetto->data_fine)) }}</b>
-        				</div>
-        			</div>
-        			<div class="row" hidable="">
-     					<div class="col-xs-2">
-        					Stato
-        				</div>
-        				<div class="col-xs-3">
-        					<b>{{ $progetto->stato }}</b>
-        				</div>
-     					<div class="col-xs-2">
-        					Data Creazione
-        				</div>
-        				<div class="col-xs-3">
-        					<b>{{  date('d/m/Y',strtotime($progetto->data_creazione)) }}</b>
-        				</div>
-        			</div>
-        			<div class="row" hidable="">
-     					<div class="col-xs-2">
-        					Budget
-        				</div>
-        				<div class="col-xs-3">
-        					<b>&euro; {{ $progetto->budget }}</b>
-        				</div>
-     					<div class="col-xs-2">
-        					Coordinatore
-        				</div>
-        				<div class="col-xs-3">
-        					<b>{{ $progetto->name }}</b>
-        				</div>
-        			</div>
-                	<div class="row" hidable="">
-     					<div class="col-xs-2">
-        					Note
-        				</div>
-        				<div class="col-xs-8">
-        					<b>{{ $progetto->note; }}</b>
-        				</div>
-        			</div>
-        			@endforeach
-        		</div>
-        	</div>
-    	</div>
-    </div>
-
-
-<div class="row">
-	<div class="col-xs-10">
-    	<div class="panel panel-default">
-        	<div class="panel-heading" id="form">
-                    Inserisci riga (click show/hide)
-            </div>
-        <div class="panel-body" > 
-        <form method="POST">
-        <div class="row">
-        	@csrf
-        		<div class="col-xs-2">
-            	
-            		<!-- Form per task con jquery e aggiunta righe automatica -->
-            		<label for="tdata" class="form-label">data:</label>
-            		 <input type="date" name="tdata" class="form-control" id="tdata" value="{{ date('Y-m-d') }}">
-            	
-            	</div>
-           	 	<div class="col-xs-5">
-            	
-            		<!-- Form per task con jquery e aggiunta righe automatica -->
-            		<label for="desc" class="form-label">descrizione:</label>
-            		 <input type="text" name="desc" class="form-control" id="desc">
-            	
-            	</div>
-            	<div class="col-xs-2">
-            		<label for="ore" class="form-label">ore lavoro:</label>
-            		<!-- Form per task con jquery e aggiunta righe automatica -->
-            		<input type="text" name="ore" class="form-control" id="ore">
-            	
-            	</div>
-         		<div class="col-xs-2">
-            		<label for="ore" class="form-label">prezzo:</label>
-            		<!-- Form per task con jquery e aggiunta righe automatica -->
-            		<input type="text" name="ore" class="form-control" id="ore">
-            	
-            	</div>
-            	<div class="col-xs-1">
-            		<input type="submit" name="Submit" class="form-control" >
-            	
-            	</div>
-            	
-        </div>
-		</form>
+	<div class="col-lg-12">
+		<h1 class="page-header">Dettaglio Progetto</h1>
 	</div>
 </div>
-</div>
-</div>
- <!-- /.col-lg-12 -->
+<div class="container">
+	<!-- Content here -->
+	<div class="row">
+		<div class="col-xs-12">
+			<a class="btn btn-primary" href="progetti/new"><i
+				class="fa fa-pencil-square-o fw"></i></a>
+		</div>
+	</div>
 
-@endsection
+	<div class="row">
+		<div class="col-xs-10">
+			<div class="panel panel-default">
+				<div class="panel-heading" id="dettaglio">Dettaglio Progetto</div>
+				<div class="panel-body">
+					@foreach($dettaglio ?? '' as $progetto)
+					<div class="row">
+						<div class="col-xs-2">Nome:</div>
+						<div class="col-xs-8">
+							<b>{{ $progetto->nome; }}</b>
+						</div>
+					</div>
 
-@section('script')
-<script>
+					<div class="row" hidable="">
+						<div class="col-xs-2">Descrizione</div>
+						<div class="col-xs-8">
+							<b>{{ $progetto->descrizione; }}</b>
+						</div>
+					</div>
+					<div class="row" hidable="">
+						<div class="col-xs-2">Data Inizio</div>
+						<div class="col-xs-3">
+							<b>{{ date('d/m/Y',strtotime($progetto->data_inizio)) }}</b>
+						</div>
+						<div class="col-xs-2">Data Termine</div>
+						<div class="col-xs-3">
+							<b>{{ date('d/m/Y',strtotime($progetto->data_fine)) }}</b>
+						</div>
+					</div>
+					<div class="row" hidable="">
+						<div class="col-xs-2">Stato</div>
+						<div class="col-xs-3">
+							<b>{{ $progetto->stato }}</b>
+						</div>
+						<div class="col-xs-2">Data Creazione</div>
+						<div class="col-xs-3">
+							<b>{{ date('d/m/Y',strtotime($progetto->data_creazione)) }}</b>
+						</div>
+					</div>
+					<div class="row" hidable="">
+						<div class="col-xs-2">Budget</div>
+						<div class="col-xs-3">
+							<b>&euro; {{ $progetto->budget }}</b>
+						</div>
+						<div class="col-xs-2">Coordinatore</div>
+						<div class="col-xs-3">
+							<b>{{ $progetto->name }}</b>
+						</div>
+					</div>
+					<div class="row" hidable="">
+						<div class="col-xs-2">Note</div>
+						<div class="col-xs-8">
+							<b>{{ $progetto->note; }}</b>
+						</div>
+					</div>
+					@endforeach
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-xs-10">
+			<div class="panel panel-default">
+				<div class="panel-heading" id="form">Inserisci riga (click
+					show/hide)</div>
+				<div class="panel-body">
+					<form method="POST">
+						<div class="row">
+							@csrf
+							<div class="col-xs-2">
+
+								<!-- Form per task con jquery e aggiunta righe automatica -->
+								<label for="tdata" class="form-label">data:</label> <input
+									type="date" name="tdata" class="form-control" id="tdata"
+									value="{{ date('Y-m-d') }}">
+
+							</div>
+							<div class="col-xs-5">
+
+								<!-- Form per task con jquery e aggiunta righe automatica -->
+								<label for="desc" class="form-label">descrizione:</label> <input
+									type="text" name="desc" class="form-control" id="desc">
+
+							</div>
+							<div class="col-xs-2">
+								<label for="ore" class="form-label">ore lavoro:</label>
+								<!-- Form per task con jquery e aggiunta righe automatica -->
+								<input type="text" name="ore" class="form-control" id="ore">
+
+							</div>
+							<div class="col-xs-2">
+								<label for="ore" class="form-label">prezzo:</label>
+								<!-- Form per task con jquery e aggiunta righe automatica -->
+								<input type="text" name="ore" class="form-control" id="ore">
+
+							</div>
+							<div class="col-xs-1">
+								<input type="submit" name="Submit" class="form-control">
+
+							</div>
+
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-10">
+			<div class="panel panel-default">
+				<div class="panel-heading" id="form">Elementi e costi del progetto</div>
+			</div>
+			<div class="panel-body">
+				<table class="table table-striped table-bordered table-hover"
+					id="tab_progetti">
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th>Descrizione</th>
+							<th>Ore lavoro</th>
+							<th>Costo</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($righe as $riga)
+						<tr>
+							<td>{{ date('d/m/Y',strtotime($riga->data)) }}</td>
+							<td>{{ $riga->descrizione }}</td>
+							<td>{{ $riga->ore }}</td>
+							<td>{{ $riga->prezzo }}</td>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<
+	</div>
+	<!-- /.col-lg-12 -->
+
+	@endsection @section('script')
+	<script>
         $(document).ready(function() {
             $('#tab_progetti').DataTable({
                     responsive: true
@@ -161,5 +170,5 @@
             	$("[hidable]").slideToggle(500);
             });
         });
-</script>
-@endsection
+	</script>
+	@endsection
