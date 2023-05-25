8895015eb22b4a1b4dd1da32711fe6e15581d11b
Flavio Barachino <flavio.barachino@gmail.com>
2023-05-25 08:05:05 +0200

    aggiornamento manutenzione.blade

--- resources/views/auto/manutenzione.blade.php
+++ resources/views/auto/manutenzione.blade.php
@@ -13,44 +13,7 @@
                 Manutenzione auto {{ $dettagli->marca;}} {{ $dettagli->modello; }} {{ $dettagli->targa; }}
             </div>
             <div class="panel-body">  	
-    			<!-- Form -->
-    			<form action="" method="POST">
-                	@csrf
-                    <div class="row">	
-                    	<div class="col-xs-6">
-                    		<label for="data" class="form-label">Data</label>
-                    		<input type="date" class="form-control" id="data" name="data" value="{{ date('Y-m-d');}}">
-                    	</div>
-                    	<div class="col-xs-6">
-             				<label for="km" class="form-label">Km</label>
-                    		<input type="text" class="form-control" id="km" name="km">
-                    	</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-6">
-                    		<label for="importo" class="form-label">Importo totale</label>
-                    		<input type="text" class="form-control" id="importo" name="importo">
-                    	</div>
-                    	<div class="col-xs-6">
-                    		<label for="descrizione" class="form-label">Descrizione</label>
-                    		<input type="text" class="form-control" id="descrizione" name="descrizione">
-                    	</div> 
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-6">
-                    		<label for="inMovimenti" class="form-label">Inserire nei movimenti?</label>
-                    		<input type="checkbox" id="inMovimenti" name="inMovimenti">
-                    	</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-12">
-                        <input type="hidden" name="type" value="manutenzione">
-                        <input type="hidden" name="auto" value="{{ $dettagli->id; }}">
-                        	<button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-            	</form>
-    			<!-- /Form -->
+    			
     		</div>
 		</div>
 	</div>
