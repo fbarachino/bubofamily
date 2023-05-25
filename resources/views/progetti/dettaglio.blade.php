259244276e46f88653dd7d76d56390ee660f8071
Flavio Barachino <flavio.barachino@gmail.com>
2023-05-25 13:28:22 +0200

    sistemato parte grafica dei popup inserimento e modifica riga progetto

--- resources/views/progetti/dettaglio.blade.php
+++ resources/views/progetti/dettaglio.blade.php
@@ -6,16 +6,15 @@
 </div>
 <div class="container">
 	<!-- Content here -->
-	
+
 
 	<div class="row">
 		<div class="col-xs-10">
 			<div class="panel panel-default">
 				<div class="panel-heading" id="dettaglio">Dettaglio Progetto</div>
 				<div class="panel-body">
-				@foreach($tot as $totale)
-				@endforeach
-					@foreach($dettaglio ?? '' as $progetto)
+					@foreach($tot as $totale) @endforeach @foreach($dettaglio ?? '' as
+					$progetto)
 					<div class="row">
 						<div class="col-xs-2">Nome:</div>
 						<div class="col-xs-8">
@@ -81,169 +80,184 @@
 
 
 	<div class="row">
-		<div class="col-xs-10">
-			&nbsp;
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-10">
-    		<div class="row p-3">
-    			<button class="btn btn-primary open_modal_addRow">Nuova riga</button>
-    		</div>	
-			<div class="row">
-				<table class="table table-striped table-bordered table-hover" id="tab_progetti">
-					<thead>
-						<tr>
-							<th>Data</th>
-							<th>Descrizione</th>
-							<th>Ore lavoro</th>
-							<th>Costo</th>
-							<th>Azioni</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($righe as $riga)
-						@if(isset($riga->data))
-						<tr>
-							<td>{{ date('d/m/Y',strtotime($riga->data))  }}</td>
-							<td>{{ $riga->descrizione }}</td>
-							<td>{{ $riga->ore }}</td>
-							<td>{{ $riga->prezzo }}</td>
-							<td><a href="/admin/progetti/delete_row/{{ $riga->id }}/return/{{ $progetto->id }}" class="btn btn-danger">X</a>
-								&nbsp;
-								<button class="btn btn-warning btn-detail open_modal" value="{{$riga->id}}">Edit</button>
-							</td>
-							
-						</tr>
-						@else
-						<tr>
-							<td>non c'è</td>
-							<td>non c'è</td>
-							<td>non c'è</td>
-							<td>non c'è</td>
-						</tr>
-						@endif
-						@endforeach
-						
-    				</tbody>
-				</table>
-				<table>
-					<tr>
-						<td colspan="2"><strong>Totale</strong></td>
-						<td></td>
-						<td><strong>{{ $totale->costo}} &euro;</strong></td>
-					</tr>
-				</table>
-			</div>
-			
-		</div>
+		<div class="col-xs-10">&nbsp;</div>
 	</div>
 </div>
-<!-- MODAL -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="row">
+	<div class="col-xs-10">
+		<div class="row p-3">
+			<button class="btn btn-primary open_modal_addRow">Nuova riga</button>
+		</div>
+		<div class="row">
+			<table class="table table-striped table-bordered table-hover"
+				id="tab_progetti">
+				<thead>
+					<tr>
+						<th>Data</th>
+						<th>Descrizione</th>
+						<th>Ore lavoro</th>
+						<th>Costo</th>
+						<th>Azioni</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($righe as $riga) @if(isset($riga->data))
+					<tr>
+						<td>{{ date('d/m/Y',strtotime($riga->data)) }}</td>
+						<td>{{ $riga->descrizione }}</td>
+						<td>{{ $riga->ore }}</td>
+						<td>{{ $riga->prezzo }}</td>
+						<td><a
+							href="/admin/progetti/delete_row/{{ $riga->id }}/return/{{ $progetto->id }}"
+							class="btn btn-danger">X</a> &nbsp;
+							<button class="btn btn-warning btn-detail open_modal"
+								value="{{$riga->id}}">Edit</button></td>
+
+					</tr>
+					@else
+					<tr>
+						<td>non c'è</td>
+						<td>non c'è</td>
+						<td>non c'è</td>
+						<td>non c'è</td>
+					</tr>
+					@endif @endforeach
+
+				</tbody>
+			</table>
+			<table>
+				<tr>
+					<td colspan="2"><strong>Totale</strong></td>
+					<td></td>
+					<td><strong>{{ $totale->costo}} &euro;</strong></td>
+				</tr>
+			</table>
+		</div>
+
+	</div>
+</div>
+
+<!-- MODAL EDIT Riga-->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" 
+	aria-labelledby="myModalLabel" aria-hidden="true">
+	<form method="POST" action="rigaupdate">
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
-				 <div class="panel-heading" id="form">Modifica Riga</div>
+				<div class="modal-header">
+					<h4 class="modal-title">Modifica Riga</h4>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						@csrf
+						<div class="col-xs-6">
+							<label for="tdata" class="form-label">data:</label> <input
+								type="date" name="data" class="form-control" id="data_u"
+								value="">
+						</div>
+						<div class="col-xs-6">
 
-				<div class="panel-body">
-					<form method="POST" action="rigaupdate">
-
-						<div class="row">
-							@csrf
-							<div class="col-xs-2">
-
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<label for="tdata" class="form-label">data:</label> <input
-									type="date" name="data" class="form-control" id="data_u"
-									value="">
-							</div>
-							<div class="col-xs-5">
-
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<label for="desc" class="form-label">descrizione:</label> <input
-									type="text" name="descrizione" class="form-control" id="desc_u"
-									value="">
-
-							</div>
-							<div class="col-xs-2">
-								<label for="ore" class="form-label">ore lavoro:</label>
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<input type="text" name="ore" class="form-control" id="ore_u">
-
-							</div>
-							<div class="col-xs-2">
-								<label for="prezzo" class="form-label">prezzo:</label>
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<input type="text" name="prezzo" class="form-control"
-									id="prezzo_u">
-
-							</div>
-							<div class="col-xs-1">
-								<input type="hidden" name="fk_id_progetto" id="id_progetto_u"> <input
-									type="hidden" name="idriga" id="idriga"> <input type="submit"
-									name="Submit" class="form-control">
-
-							</div>
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<label for="desc" class="form-label">descrizione:</label> <input
+								type="text" name="descrizione" class="form-control" id="desc_u"
+								value="">
 
 						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6">
+							<label for="ore" class="form-label">ore lavoro:</label>
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<input type="text" name="ore" class="form-control" id="ore_u">
 
-					</form>
-				 </div>
+						</div>
+						<div class="col-xs-6">
+							<label for="prezzo" class="form-label">prezzo:</label>
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<input type="text" name="prezzo" class="form-control"
+								id="prezzo_u">
+
+						</div>
+					</div>
+				</div>
+
+				<div class="modal-footer">
+					<div class="col-xs-12">
+						<input type="hidden" name="fk_id_progetto" id="id_progetto_u"> <input
+							type="hidden" name="idriga" id="idriga"> <input type="submit"
+							name="Submit" class="form-control" value="Aggiorna">
+
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</form>
+</div>
+
+<!-- MODAL END -->
+
+<!-- MODAL NEW ROW -->
+<div class="modal fade" id="myModal_addRow" tabindex="-1" role="dialog"
+	aria-labelledby="myModalLabel" aria-hidden="true">
+	<form method="POST" action="">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Aggiungi riga</h4>
+				</div>
+				<div class="modal-body">
+
+					<div class="row">
+						@csrf
+						<div class="col-xs-6">
+
+
+							<label for="tdata" class="form-label">data:</label> <input
+								type="date" name="data" class="form-control" id="data"
+								value="{{ date('Y-m-d') }}">
+
+						</div>
+						<div class="col-xs-6">
+
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<label for="desc" class="form-label">descrizione:</label> <input
+								type="text" name="descrizione" class="form-control" id="desc">
+
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6">
+							<label for="ore" class="form-label">ore lavoro:</label>
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<input type="text" name="ore" class="form-control" id="ore">
+
+						</div>
+						<div class="col-xs-6">
+							<label for="ore" class="form-label">prezzo:</label>
+							<!-- Form per task con jquery e aggiunta righe automatica -->
+							<input type="text" name="prezzo" class="form-control" id="ore">
+
+						</div>
+					</div>
+				</div>
+
+				<div class="modal-footer">
+					<div class="col-xs-12">
+						<input type="submit" name="Submit" class="form-control" value="Inserisci">
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<!-- MODAL END -->
-	
-	<!-- MODAL NEW ROW -->
-	<div class="modal fade" id="myModal_addRow" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				 <div class="panel-heading" id="form">Aggiungi Riga</div>
-	
-					<form method="POST" action="">
-						<div class="row">
-							@csrf
-							<div class="col-xs-2">
+	</form>
+</div>
 
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<label for="tdata" class="form-label">data:</label> <input
-									type="date" name="data" class="form-control" id="data"
-									value="{{ date('Y-m-d') }}">
 
-							</div>
-							<div class="col-xs-5">
+<!-- /MODAL RIGA -->
 
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<label for="desc" class="form-label">descrizione:</label> <input
-									type="text" name="descrizione" class="form-control" id="desc">
 
-							</div>
-							<div class="col-xs-2">
-								<label for="ore" class="form-label">ore lavoro:</label>
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<input type="text" name="ore" class="form-control" id="ore">
+<!-- /.col-lg-12 -->
 
-							</div>
-							<div class="col-xs-2">
-								<label for="ore" class="form-label">prezzo:</label>
-								<!-- Form per task con jquery e aggiunta righe automatica -->
-								<input type="text" name="prezzo" class="form-control" id="ore">
-
-							</div>
-							<div class="col-xs-1">
-								<!--  <input type="hidden" name="fk_id_progetto" value="">-->
-								<input type="submit" name="Submit" class="form-control">
-
-							</div>
-
-						</div>
-					</form>
-	<!-- /MODAL RIGA -->
-	
-	
-	<!-- /.col-lg-12 -->
-
-	@endsection @section('script')
-	<script src="/js/app/rigaProgetti.js"></script>
-	@endsection
+@endsection @section('script')
+<script src="/js/app/rigaProgetti.js"></script>
+@endsection
