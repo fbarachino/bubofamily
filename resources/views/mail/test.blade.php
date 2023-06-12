f22943403a61c84265db417f2c9367df405760a8
Flavio Barachino <flavio.barachino@gmail.com>
2023-06-12 15:01:18 +0200

    rivisto template email di test

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -1,4 +1,11 @@
+@component('mail::message')
+
 Ciao {{ Auth::user()->name ?? $name }},
 Hai configurato correttamente la posta elettronica!
-
+@component('mail::button', ['url' => 'https://gestionale.lavorain.cloud/'])
 Ciao.
+@endcomponent
+
+Buona giornata,<br>
+{{ config('app.name') }}
+@endcomponent
