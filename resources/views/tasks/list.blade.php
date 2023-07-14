350c5737b7da3acb49d5631508fa6016220d0935
FlavioBarachino <flavio.barachino@gmail.com>
2023-07-14 16:19:40 +0200

    Task Controllers

---
+++ resources/views/tasks/list.blade.php
@@ -0,0 +1,16 @@
+@extends('admin') 
+@section('content')
+<div class="row">
+	<div class="col-lg-12">
+		<h1 class="page-header">Lista Progetti</h1>
+	</div>
+</div>
+<div class="container">
+	<!-- Content here -->
+	<div class="row">
+		<div class="col-xs-12">
+			<button class="btn btn-primary open_modal_new"><i
+				class="fa fa-pencil-square-o fw"></i>Nuovo Progetto</button>
+		</div>
+	</div>
+    @endsection
